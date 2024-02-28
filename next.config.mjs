49b21a716285827f1d6f85df81527269e778effe
Dan Ugelow <dan.ugelow@gmail.com>
2024-02-27 19:46:42 -0500

    configure edge for cloudfare

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // runtime edge for cloudfare deployment
+  experimental: {
+    runtime: "edge",
+  },
 };
 
 export default nextConfig;
