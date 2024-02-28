767c9e0808f5038e7912d90776b52655724dd941
Dan Ugelow <dan.ugelow@gmail.com>
2024-02-27 19:52:13 -0500

    remove api

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // runtime edge for cloudfare deployment
-  experimental: {
-    runtime: "edge",
-  },
 };
 
 export default nextConfig;
